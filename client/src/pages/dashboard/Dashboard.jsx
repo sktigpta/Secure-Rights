179e2981aafb685db9a8d65eecdb456ca30d5f11
Shaktidhar Gupta <sktigpta@gmail.com>
2025-03-25 15:13:37 +0530

    Fixed dependencies issue

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Dashboard.css";
 import FetchedVideos from "../../components/fetched-videos/FetchedVideos";
 import { ContainerTwo } from "../../components/cards-and-containers/Container";
-import Query from "../../components/query/query";
 import PermittedVideos from "../../components/permitted-channel/permittedChannel";
 import Processed from "../../components/processed-video/Processed";
 import Terminal from "../../components/terminal/Terminal";
+import Query from "../../components/query/query";
 
 function Dashboard() {
     return (
