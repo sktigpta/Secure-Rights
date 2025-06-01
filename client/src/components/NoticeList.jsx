6e6f1749390dd47394bc892d0d908e03dc1d3e85
Satyam kumar <jhajhasatyam100@gmail.com>
2025-06-01 10:48:35 +0530

    fix

--- client/src/components/NoticeList.jsx
+++ client/src/components/NoticeList.jsx
@@ -18,7 +18,6 @@ function NoticeList() {
   const fetchNotices = async () => {
     try {
       const API_URL = import.meta.env.VITE_API_URL;
-  
       const response = await axios.get(`${API_URL}/dmca/notices`, {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('authToken')}`
